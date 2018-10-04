739ee8a3139e13b364d8762979ee96860fcca2ea
Evgenii Vetoshkin <overevgen@mail.ru>
2018-10-04 17:26:19 +0300

    финал

--- Lesson_1_dop/scripts.js
+++ Lesson_1_dop/scripts.js
@@ -1,12 +1,15 @@
-"use strict";
+function start() {
+    "use strict";
 
-var num = 33721;
-var strNum = num + "";
-var multipleNum = strNum[0] * strNum[1] * strNum[2] * strNum[3] * strNum[4];
+    var num = 33721;
+    var strNum = num + "";
+    var multipleNum = strNum[0] * strNum[1] * strNum[2] * strNum[3] * strNum[4];
 
-console.log(multipleNum);
+    console.log(multipleNum);
 
-var degreeMultipleNum = multipleNum ** 3;
-var strDegreeMultipleNum = degreeMultipleNum + "";
+    var degreeMultipleNum = multipleNum ** 3;
+    var strDegreeMultipleNum = degreeMultipleNum + "";
 
-alert(strDegreeMultipleNum[0] + strDegreeMultipleNum[1]);
+    alert(strDegreeMultipleNum[0] + strDegreeMultipleNum[1]);
+};
+start();
