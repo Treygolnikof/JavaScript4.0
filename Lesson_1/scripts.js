89067f0484c35c30ac99969f2c6eab69d16c49fa
Evgenii Vetoshkin <overevgen@mail.ru>
2018-10-04 16:39:56 +0300

    Финал

---
+++ Lesson_1/scripts.js
@@ -0,0 +1,25 @@
+"use strict";
+
+var money = prompt("Ваш бюджет на месяц?", "");
+var time = prompt("Введите дату в формате YYYY-MM-DD", "");
+
+var answerExpenses1 = prompt("Введите обязательную статью расходов в этом месяце", "");
+var answerExpensesBudget1 = prompt("Во сколько обойдется?", "");
+var answerExpenses2 = prompt("Введите обязательную статью расходов в этом месяце", "");
+var answerExpensesBudget2 = prompt("Во сколько обойдется?", "");
+
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {
+        [answerExpenses1]: answerExpensesBudget1,
+        [answerExpenses2]: answerExpensesBudget2
+    },
+    optionalExpenses: {},
+    income: [],
+    savings: false
+};
+
+let dailyBudget = money / 30;
+
+alert("Ваш бюджет на день составляет " + dailyBudget + " рублей");
---
+++ Lesson_1_dop/scripts.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var num = 33721;
+var multipleNum = num * num;
+
+console.log(multipleNum);
+
+var degreeMultipleNum = multipleNum ** 3;
+var str = degreeMultipleNum + "";
+
+console.log( str[0] + str[1] );
