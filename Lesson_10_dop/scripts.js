0d92e49092935d023bda5f6dfca67b3b9d735b92
Evgenii Vetoshkin <overevgen@mail.ru>
2018-10-20 21:26:00 +0300

    Final

--- Lesson_10_dop/scripts.js
+++ Lesson_10_dop/scripts.js
@@ -1,8 +1,9 @@
 let phonenumber = document.getElementById('phonenumber');
 
 phonenumber.addEventListener('keyup', function() {
-    phonenumber.maxLength = 14;
+    phonenumber.maxLength = 17;
+    phonenumber.value = phonenumber.value.replace('+7', "");
     phonenumber.value = phonenumber.value.replace(/\D/g, "");
     phonenumber.value = phonenumber.value.replace(/^(\d{3})(\d)/g, "($1) $2");
-    phonenumber.value = phonenumber.value.replace(/(\d{3})(\d)/, "$1-$2");
+    phonenumber.value = '+7 ' + phonenumber.value.replace(/(\d{3})(\d)/, "$1-$2");
 });
